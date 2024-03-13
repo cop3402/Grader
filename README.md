5217e0c079798943a4e5e641001288a38b2777cf
Sai Naga Sri Harshita Manthena <156922002+harshitamsns21@users.noreply.github.com>
2024-03-13 09:57:13 -0400

    Grader script

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Grader
+# grader-scripts
+
+
+## Usage
+
+Run the following command: `python3 testSimplec.py path/to/your/repo path/to/the/tests keepBinaries compileAndCheckRetValue`
+
+Example: `python3 testSimplec.py /vagrant/simplec-compiler-josh /vagrant/tests-cases-public/proj1 true true` 
+
+Set the second to last arg to true if you are using precompiled binaries from ealier projects
+Set the last arg to false for projects 1-2 and true for 3-4. It will compile your assembly output with gcc, run the binary, and check for the return value
+
+
+Sample output:
+
+```
+# building your simplec compiler
+Build succeeded
+Testing example.simplec: Failure. See example.simplec.diff for diff and example.simplec.out for output.
+Testing functioncall1.simplec: Success!
+Testing simple.simplec: Success!
+Testing ifelse.simplec: Success!
+Testing if.simplec: Success!
+Testing assign.simplec: Success!
+Testing quicksort.simplec: Failure. See quicksort.simplec.diff for diff and quicksort.simplec.out for output.
+Testing binary_ops.simplec: Success!
+Testing call.simplec: Success!
+Testing math1.simplec: Success!
+Testing read.simplec: Failure. See read.simplec.diff for diff and read.simplec.out for output.
+Testing digitcount.simplec: Success!
+Testing battleship.simplec: Failure. See battleship.simplec.diff for diff and battleship.simplec.out for output.
+Testing negation.simplec: Success!
+Testing badtype.simplec: Success!
+Testing sudoku.simplec: Failure. See sudoku.simplec.diff for diff and sudoku.simplec.out for output.
+11 / 16 test casing passing. 
+```
---
+++ testSimplec.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import glob
+import shutil
+import subprocess
+from lib import cd, Submission, run_cmd
+
+source_path = os.path.dirname(os.path.abspath(__file__)) # /a/b/c/d/e
+test_case_points = 1
+build_points = 1 # points for building. tentative
+submission_points = 1
+
+def buildAndTest(submissionpath, sourceTestPath, no_remove, gcc=False):
+    points = submission_points
+    script_path = os.path.dirname(os.path.realpath(__file__))
+
+    # create temporary directory so that previous students' results will not affect subsequent tests
+    testCasePath = sourceTestPath
+
+    testCases = glob.glob(os.path.join(testCasePath, "*.simplec"))
+    #print(f"testCases {testCases}")
+    if not no_remove:
+        for i in glob.glob(os.path.join(submissionpath, "*.o")):
+            if os.path.exists(i):
+                os.remove(i)
+    progname = os.path.join(submissionpath, "simplec")
+    if os.path.exists(progname):
+        os.remove(progname)
+
+    if len(testCases) == 0:
+        print("# no tests found.  double-check your path: " + testCasePath)
+        sys.exit()
+
+
+    if os.path.exists(submissionpath + "/simplec"):
+        os.remove(submissionpath + "/simplec")
+    out = subprocess.run(['make'], cwd = submissionpath,
+            stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+
+    output = ""
+    err = ""
+    if out.returncode != 0:
+        output += "Make failed."  
+        print(output + " Do you have a Makefile?") # can't even compile the compiler 
+        return 0, output 
+    else:
+        print("Build succeeded!")
+        points += build_points # points to build. tentative   
+    
+    # simpleC compilers lives so lets go through every test case now
+    for case in testCases:
+        base_name = os.path.basename(case)
+        ground_truth = case.replace(".simplec", ".ast")
+        if gcc:
+            ground_truth = case.replace(".simplec", ".out")
+        output_file = base_name.replace(".simplec", ".out")
+        diff_file = base_name.replace(".simplec", ".diff")
+        print(f"Testing {base_name}:", end=" ")
+
+        with cd(submissionpath):
+            
+            if gcc:
+                cmd = f"cat \"{case}\" | ./simplec > {output_file}.s"
+                return_code, stdout_, stderr_ = run_cmd(cmd)
+                
+                cmd = f"gcc -o {output_file}.bin {output_file}.s "
+                return_code, stdout_, stderr_ = run_cmd(cmd)
+                
+                cmd = f"./{output_file}.bin"
+                return_code, stdout_, stderr_ = run_cmd(cmd)
+                
+                # open a (new) file to write
+                fp = open(output_file, "w")
+                fp.write(f"{return_code}")
+                fp.close()
+
+
+            else:
+                cmd = f"cat \"{case}\" | ./simplec > {output_file}"
+            return_code, stdout_, stderr_ = run_cmd(cmd)
+            cmd = f"diff -w -B \"{ground_truth}\" {output_file}"
+            print(f"Running command: {cmd}")
+            return_code, stdout_, stderr_ = run_cmd(cmd,False)
+            if return_code == 0 and len(stdout_) == 0:
+                print("Success!")
+                points += test_case_points
+            if return_code == 1 and len(stdout_) > 0:
+                print(f"Failure. See {diff_file} for diff and {output_file} for output.")
+                diff_out = open(diff_file, "w")
+                diff_out.write(stdout_)
+                diff_out.close()
+
+                return_code, stdout_, stderr_ = run_cmd(cmd,False)
+            if return_code > 1:
+                print(f"diff exited with an unknown return code. This shouldn't happen. Here is the stderr: {stderr_}")
+    print(f"{int((points - build_points) / test_case_points)} / {len(testCases)} test casing passing. ")
+    print(f"Points awarded - {points} ")
+    return points, output 
+
+def error(app, f):
+
+    return "# ERROR " + app + " failed on " + f + "\n"
+    
+
+if __name__ == "__main__":
+
+    try:
+        submissionDirectory = os.path.abspath(sys.argv[1])
+        sourceTestPath = os.path.abspath(sys.argv[2])
+        norm = sys.argv[3]
+        usegcc = sys.argv[4]
+    except:
+        print("USAGE: path/to/your/repo path/to/the/tests")
+        print("example: ./ ../syllabus/projects/tests/proj0/")
+        sys.exit()
+    no_remove = True if norm == "true" else False
+    gcc = True if usegcc == "true" else False
+    buildAndTest(submissionDirectory, sourceTestPath, no_remove, gcc)
